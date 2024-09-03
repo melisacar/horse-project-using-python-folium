24082fe56786b4f6008eca4a97a0a809d5b7e390
Melisa <acarme17@itu.edu.tr>
2024-09-03 16:24:26 +0300

    feat(map): add heatmap and markers with research details

---
+++ regional-heat-map-project/main.py
@@ -0,0 +1,34 @@
+import folium 
+import pandas as pd
+from folium.plugins import HeatMap
+
+#Latitude and longitude information of locations and research numbers in each location
+locations = [
+    {'name': 'Location1', 'lat': 41.02611, 'lon': 29.00000, 'research_count': 100},
+    {'name': 'Location2', 'lat': 40.90278, 'lon': 29.05250, 'research_count': 10},
+    {'name': 'Location3', 'lat': 40.42723, 'lon': 26.73223, 'research_count': 40}  
+    #Other locations
+]
+
+#Create dataframe with pandas
+#df = pd.DataFrame(data)
+
+#Creating the map
+heat_map = folium.Map(location=[40.63, 28.12], tiles='Cartodb Positron', zoom_start=8)
+
+#Data points for the heatmap
+heat_data = [[loc['lat'], loc['lon'], loc['research_count']] for loc in locations]
+
+#Adding the heatmap
+HeatMap(heat_data).add_to(heat_map)
+
+#Adding markers
+for loc in locations:
+    folium.Marker(
+        location=[loc['lat'], loc['lon']],
+        popup=f"{loc['name']}: {loc['research_count']} studies",
+        tooltip=loc['name'],
+        icon=folium.Icon(color='black', icon='info-sign')
+    )
+
+heat_map.save("heat_map.html")
